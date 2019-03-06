bd2c4ed2e3cac22c076364f31dae853d1054e93f
Chypriote <nicolastem@hotmail.fr>
2019-03-06 13:48:00 +0100

    solary mces

--- src/store.js
+++ src/store.js
@@ -81,8 +81,8 @@ function initialState() {
 				matchs: [
 					{ id: 711, team_blue: 1, team_red: 2, winner: null },
 					{ id: 712, team_blue: 4, team_red: 5, winner: null },
-					{ id: 713, team_blue: 3, team_red: 7, winner: null },
-					{ id: 714, team_blue: 8, team_red: 6, winner: null },
+					{ id: 713, team_blue: 3, team_red: 8, winner: null },
+					{ id: 714, team_blue: 7, team_red: 6, winner: null },
 				],
 			},
 			{
@@ -91,8 +91,8 @@ function initialState() {
 				matchs: [
 					{ id: 721, team_blue: 4, team_red: 2, winner: null },
 					{ id: 722, team_blue: 3, team_red: 1, winner: null },
-					{ id: 723, team_blue: 8, team_red: 5, winner: null },
-					{ id: 724, team_blue: 6, team_red: 7, winner: null },
+					{ id: 723, team_blue: 7, team_red: 5, winner: null },
+					{ id: 724, team_blue: 6, team_red: 8, winner: null },
 				],
 			},
 			{
@@ -100,9 +100,9 @@ function initialState() {
 				day: 1,
 				matchs: [
 					{ id: 811, team_blue: 3, team_red: 2, winner: null },
-					{ id: 812, team_blue: 8, team_red: 4, winner: null },
+					{ id: 812, team_blue: 7, team_red: 4, winner: null },
 					{ id: 813, team_blue: 6, team_red: 1, winner: null },
-					{ id: 814, team_blue: 7, team_red: 5, winner: null },
+					{ id: 814, team_blue: 8, team_red: 5, winner: null },
 				],
 			},
 		],
