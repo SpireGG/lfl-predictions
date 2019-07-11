4936cbe67273e10413f44475e8fb367f2f553d74
NicolasT <nicolastem@hotmail.fr>
2019-07-11 13:29:23 +0200

    update week 7

--- src/static/lfl/weeks.js
+++ src/static/lfl/weeks.js
@@ -1,25 +1,5 @@
 export const weeks = [
 	{
-		number: 7,
-		day: 1,
-		matchs: [
-			{ id: 711, team_blue: 5, team_red: 1, winner: null },
-			{ id: 712, team_blue: 8, team_red: 7, winner: null },
-			{ id: 713, team_blue: 2, team_red: 6, winner: null },
-			{ id: 714, team_blue: 3, team_red: 4, winner: null },
-		],
-	},
-	{
-		number: 7,
-		day: 2,
-		matchs: [
-			{ id: 721, team_blue: 7, team_red: 4, winner: null },
-			{ id: 722, team_blue: 3, team_red: 5, winner: null },
-			{ id: 723, team_blue: 2, team_red: 1, winner: null },
-			{ id: 724, team_blue: 8, team_red: 6, winner: null },
-		],
-	},
-	{
 		number: 8,
 		day: 1,
 		matchs: [
