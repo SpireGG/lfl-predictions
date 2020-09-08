35b98e24cdd0159c06c9108ca3a98051a78b0c4b
NicolasT <nicolastem@hotmail.fr>
2020-09-08 21:35:08 +0200

    upgrade and vita win

--- src/static/lfl/weeks.js
+++ src/static/lfl/weeks.js
@@ -5,7 +5,7 @@ export const weeks = [
 		matchs: [
 			{ id: 521, team_blue: 8, team_red: 2, winner: 2 },
 			{ id: 522, team_blue: 5, team_red: 1, winner: 5 },
-			{ id: 523, team_blue: 6, team_red: 3, winner: null },
+			{ id: 523, team_blue: 6, team_red: 3, winner: 6 },
 			{ id: 524, team_blue: 7, team_red: 4, winner: null },
 		],
 	},
