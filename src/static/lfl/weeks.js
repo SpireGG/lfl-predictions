48b725ef54c2b8e542684f459b69b3e38295b218
NicolasT <nicolastem@hotmail.fr>
2020-09-08 21:18:15 +0200

    update summer

--- src/static/lfl/weeks.js
+++ src/static/lfl/weeks.js
@@ -1,12 +1,32 @@
 export const weeks = [
 	{
-		number: 8,
+		number: 5,
+		day: 2,
+		matchs: [
+			{ id: 521, team_blue: 8, team_red: 2, winner: 2 },
+			{ id: 522, team_blue: 5, team_red: 1, winner: 5 },
+			{ id: 523, team_blue: 6, team_red: 3, winner: null },
+			{ id: 524, team_blue: 7, team_red: 4, winner: null },
+		],
+	},
+	{
+		number: 6,
 		day: 1,
 		matchs: [
-			{ id: 811, team_blue: 8, team_red: 2, winner: null },
-			{ id: 812, team_blue: 4, team_red: 6, winner: null },
-			{ id: 813, team_blue: 3, team_red: 1, winner: null },
-			{ id: 814, team_blue: 7, team_red: 5, winner: null },
+			{ id: 611, team_blue: 5, team_red: 6, winner: null },
+			{ id: 612, team_blue: 7, team_red: 3, winner: null },
+			{ id: 613, team_blue: 8, team_red: 1, winner: null },
+			{ id: 614, team_blue: 4, team_red: 2, winner: null },
+		],
+	},
+	{
+		number: 6,
+		day: 2,
+		matchs: [
+			{ id: 621, team_blue: 7, team_red: 5, winner: null },
+			{ id: 622, team_blue: 8, team_red: 6, winner: null },
+			{ id: 623, team_blue: 4, team_red: 3, winner: null },
+			{ id: 624, team_blue: 1, team_red: 2, winner: null },
 		],
 	},
 ];
